e61b91253419459ce4b0f25f68a15c1aaf90b343
Artemova <artemovaok84@mail.ru>
2023-06-07 23:51:49 +0300

    ReadyTests

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -17,4 +17,76 @@ public class CreditAccountTest {
 
         Assertions.assertEquals(3_000, account.getBalance());
     }
+
+    @Test
+    public void shouldAddToNegativeBalance() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                10
+        );
+
+        account.add(-15_000);
+
+        Assertions.assertEquals(0, account.getBalance());
+    }
+
+    @Test
+    public void shouldMoneyAddBalance() {
+        CreditAccount account = new CreditAccount(
+                5_000,
+                5_000,
+                10
+        );
+
+        account.add(3_000);
+
+        Assertions.assertEquals(8_000, account.getBalance());
+    }
+
+    @Test
+    public void shouldYearChangeNegativeTest() {
+        CreditAccount account = new CreditAccount(
+                -200,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(-30, account.yearChange());
+    }
+
+    @Test
+    public void shouldYearChangePositiveTest() {
+        CreditAccount account = new CreditAccount(
+                200,
+                5_000,
+                15
+        );
+
+        Assertions.assertEquals(0, account.yearChange());
+    }
+
+    @Test
+    public void payTest() {
+        CreditAccount account = new CreditAccount(
+                8_000,
+                5_000,
+                10
+        );
+
+        account.pay(3_000);
+
+        Assertions.assertEquals(5_000, account.getBalance());
+    }
+
+    @Test
+    public void payLimitTest() {
+        CreditAccount account = new CreditAccount(
+                0,
+                5_000,
+                10
+        );
+        Assertions.assertFalse(account.pay(8_000));
+
+    }
 }
