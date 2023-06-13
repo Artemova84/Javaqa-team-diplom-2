eec5b06567117ef05361c9026cccecbcd062acae
Artemova84 <129989638+Artemova84@users.noreply.github.com>
2023-06-13 10:38:23 +0300

    Update CreditAccountTest.java

--- src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
+++ src/test/java/ru/netology/javaqadiplom/CreditAccountTest.java
@@ -8,6 +8,7 @@ public class CreditAccountTest {
     @Test
     public void shouldAddToPositiveBalance() {
         CreditAccount account = new CreditAccount(
+                3_000,
                 0,
                 5_000,
                 15
@@ -22,6 +23,7 @@ public class CreditAccountTest {
     public void shouldAddToNegativeBalance() {
         CreditAccount account = new CreditAccount(
                 0,
+                0,
                 5_000,
                 10
         );
@@ -34,6 +36,7 @@ public class CreditAccountTest {
     @Test
     public void shouldMoneyAddBalance() {
         CreditAccount account = new CreditAccount(
+                8_000,
                 5_000,
                 5_000,
                 10
@@ -47,6 +50,7 @@ public class CreditAccountTest {
     @Test
     public void shouldYearChangeNegativeTest() {
         CreditAccount account = new CreditAccount(
+                -230,
                 -200,
                 5_000,
                 15
@@ -59,6 +63,7 @@ public class CreditAccountTest {
     public void shouldYearChangePositiveTest() {
         CreditAccount account = new CreditAccount(
                 200,
+                200,
                 5_000,
                 15
         );
@@ -69,6 +74,7 @@ public class CreditAccountTest {
     @Test
     public void payTest() {
         CreditAccount account = new CreditAccount(
+                5_000,
                 8_000,
                 5_000,
                 10
@@ -77,5 +83,17 @@ public class CreditAccountTest {
         account.pay(3_000);
 
         Assertions.assertEquals(5_000, account.getBalance());
+    }
+
+    @Test
+    public void payLimitTest() {
+        CreditAccount account = new CreditAccount(
+                0,
+                0,
+                5_000,
+                10
+        );
+        Assertions.assertFalse(account.pay(8_000));
+
     }
 }
